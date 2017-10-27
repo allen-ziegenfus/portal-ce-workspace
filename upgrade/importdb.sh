1ec6203dfdadb1d229ab61354eaef77f83dad397
allen-ziegenfus <allen.ziegenfus@liferay.com>
2017-10-27 13:44:04 +0200

    add

--- upgrade/importdb.sh
+++ upgrade/importdb.sh
@@ -1,3 +1,6 @@
-
-mysql  --protocol=tcp --host=localhost --user=root --port=3306 --default-character-set=utf8 --comments --database=lportal_2017_10_24  < "/Users/allenziegenfus/Downloads/www_lportal-2017-10-24_19-00-PDT.sql"
+DATABASE=lportal_2017_10_24
+IMPORT=/Users/allenziegenfus/Downloads/www_lportal-2017-10-24_19-00-PDT.sql
+mysql -u root --execute="DROP DATABASE IF EXISTS ${DATABASE}" 
+mysql -u root --execute="CREATE DATABASE ${DATABASE}" 
+mysql  --protocol=tcp --host=localhost --user=root --port=3306 --default-character-set=utf8 --comments --database=${DATABASE} < "$IMPORT"
 
