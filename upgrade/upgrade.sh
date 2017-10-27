1f843753757845b0a9389282d0890a6495bcfb7b
allen-ziegenfus <allen.ziegenfus@liferay.com>
2017-10-27 12:15:05 +0200

    upgrade

--- upgrade/upgrade.sh
+++ upgrade/upgrade.sh
@@ -1,2 +1,3 @@
+CATALINA_OPTS="$CATALINA_OPTS -Dfile.encoding=UTF8 -Djava.net.preferIPv4Stack=true -Dorg.apache.catalina.loader.WebappClassLoader.ENABLE_CLEAR_REFERENCES=false -Duser.timezone=GMT -Xmx4096m -XX:MaxPermSize=768m"
 java -Xms4096m -Xmx4096m -XX:MaxNewSize=2048m -XX:MaxPermSize=1024m -XX:PermSize=1024m  -jar ../bundles/tools/portal-tools-db-upgrade-client/com.liferay.portal.tools.db.upgrade.client.jar 
 
