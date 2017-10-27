a20977cb1c258c3a8d661630605dff1b443afb72
allen-ziegenfus <allen.ziegenfus@liferay.com>
2017-10-27 11:54:35 +0200

    upgrade settings

---
+++ upgrade/upgrade.sh
@@ -0,0 +1,2 @@
+java -Xms4096m -Xmx4096m -XX:MaxNewSize=2048m -XX:MaxPermSize=1024m -XX:PermSize=1024m  -jar ../bundles/tools/portal-tools-db-upgrade-client/com.liferay.portal.tools.db.upgrade.client.jar 
+
